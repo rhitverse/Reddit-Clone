a39cf6598f320530850fd8793973c65a8f400ad8
rhitverse <rohitsainii.work@gmail.com>
2025-11-03 01:07:09 +0530

    fifth Commit

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 
@@ -10,7 +12,7 @@ class SignInButton extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-        onPressed: () {},
+        onPressed: () async {},
         icon: Image.asset(Constants.googlePath, width: 35),
         label: const Text(
           'Continue with Google',
@@ -20,7 +22,7 @@ class SignInButton extends StatelessWidget {
           backgroundColor: Pallete.greyColor,
           minimumSize: const Size(double.infinity, 50),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30),
+            borderRadius: BorderRadius.circular(20),
           )
         ),
       ),

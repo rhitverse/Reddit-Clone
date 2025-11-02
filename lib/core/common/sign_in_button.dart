ca52fe96f0a2e93d2b054057f8eb1456dfc67697
rhitverse <rohitsainii.work@gmail.com>
2025-11-03 03:45:21 +0530

    Sixth Commit

--- lib/core/common/sign_in_button.dart
+++ lib/core/common/sign_in_button.dart
@@ -1,18 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/constants/constants.dart';
+import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 
-class SignInButton extends StatelessWidget {
+class SignInButton extends ConsumerWidget {
   const SignInButton({super.key});
 
+  void signInWithGoogle(WidgetRef ref) {
+    ref.read(authControllerProvider).signInWithGoogle();
+  }
+
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Padding(
       padding: const EdgeInsets.all(18.0),
       child: ElevatedButton.icon(
-        onPressed: () async {},
+        onPressed: () => signInWithGoogle(ref),
         icon: Image.asset(Constants.googlePath, width: 35),
         label: const Text(
           'Continue with Google',
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/screen/login_screen.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 
 import 'firebase_options.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const MyApp());
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: MyApp(),),);
 }
 
 class MyApp extends StatelessWidget {

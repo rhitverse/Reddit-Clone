9e07f2164b4e2c4c0beed60c03b57c3944423b20
rhitverse <rohitsainii.work@gmail.com>
2025-11-04 21:21:50 +0530

    thirdteen Commit

--- lib/features/home/screen/home_screen.dart
+++ lib/features/home/screen/home_screen.dart
@@ -7,7 +7,7 @@ class HomeScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final user = ref.watch(userProvider);
+    final user = ref.watch(userProvider)!;
 
     return Scaffold(body: Center(child: Text(user?.name ?? '')));
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/common/error_text.dart';
 import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
+import 'package:reddit_clone/models/user_model.dart';
 import 'package:reddit_clone/router.dart';
 import 'package:reddit_clone/theme/pallete.dart';
 import 'package:routemaster/routemaster.dart';
@@ -17,11 +19,27 @@ void main() async {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends ConsumerWidget {
+class MyApp extends ConsumerStatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<ConsumerStatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends ConsumerState<MyApp> {
+  UserModel? userModel;
+
+  void getData(WidgetRef ref, User data) async {
+    userModel = await ref
+        .watch(authControllerProvider.notifier)
+        .getUserData(data.uid)
+        .first;
+    ref.read(userProvider.notifier).update((state) => userModel);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return ref
         .watch(authStateChangeProvider)
         .when(
@@ -32,7 +50,10 @@ class MyApp extends ConsumerWidget {
             routerDelegate: RoutemasterDelegate(
               routesBuilder: (context) {
                 if (data != null) {
-                  return loggedInRoute;
+                  getData(ref, data);
+                  if (userModel != null) {
+                    return loggedInRoute;
+                  }
                 }
                 return loggedOutRoute;
               },

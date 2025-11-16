1926b5a11bbca4380437b4220dbc16a1f14efc7b
rhitverse <rohitsainii.work@gmail.com>
2025-11-16 22:15:40 +0530

    Forty six Commit

--- lib/features/feed/feed_screens.dart
+++ lib/features/feed/feed_screens.dart
@@ -3,7 +3,6 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:reddit_clone/core/common/error_text.dart';
 import 'package:reddit_clone/core/common/loader.dart';
 import 'package:reddit_clone/core/common/post_card.dart';
-import 'package:reddit_clone/features/auth/controller/auth_controller.dart';
 import 'package:reddit_clone/features/community/controller/community_controller.dart';
 import 'package:reddit_clone/features/post/controller/post_controller.dart';
 
@@ -12,7 +11,6 @@ class FeedScreens extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-     final user = ref.watch(userProvider)!;
     return ref
         .watch(userCommunitiesProvider)
         .when(
